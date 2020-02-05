a84827ca5dac6a9689669b926888927c4333fbf6
MandeepVishwakarma <Mandeep.vishwakarma@rampgroup.com>
2020-02-05 19:19:28 +0530

    home activity added

---
+++ app/src/main/java/com/mr/zarasamplep/ui/main/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.mr.zarasamplep.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mr.zarasamplep.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
